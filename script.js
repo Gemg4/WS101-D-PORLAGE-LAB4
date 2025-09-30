4557a3c813cc4e0073874e855dc9e75c136cc2e7
Gem <jhemricporlage14@gmail.com>
2025-09-30 09:37:24 +0800

    Almost finished Js project

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 // Auction Bidding System 
-
-
-let bidderList = []
-
 // Create Bidder Object
 function createbidder(bidder) {
     const obj = {}
@@ -13,41 +9,101 @@ function createbidder(bidder) {
     return obj
 }
 
+function setItemList(bidderList) {
+    return [...new Set(bidderList.map(element => element.item))]
+}
 
 // Find the highest bid for each item
 function findHighestBid(bidderList) {
-    highestBidItems = []
+    let listOfItemAndAmount = []
+    let listOfItem = setItemList(bidderList)
 
+    for (let i = 0; i < listOfItem.length; i++) {
+        let listOfItemAmount = []
+        for (let j = 0; j < bidderList.length; j++) {
+            if (listOfItem[i] === bidderList[j].item) {
+                listOfItemAmount.push(bidderList[j].amount)
+            }
+        }
+        listOfItemAndAmount[i] = { [listOfItem[i]]: getArrayMax(listOfItemAmount) }
+    }
+    return listOfItemAndAmount
 }
 
-// Filter bidder by its attributes
-function filterBid(toFilter) {
-    bidderList.filter((element) => {
-        return element.toFilter == toFilter
-    })
+// Get max value in an array
+function getArrayMax(array) {
+    return array.reduce((acc, element) => (acc > element) ? acc : element, 0)
 }
+
+
 
 // Calculate total bids for a specific item
-function totalBids(bidder) {
-    item = bidder.item
-    bidderList.forEach(element => {
-        
-    });
+function calculateTotalBids(bidderList) {
+    let listOfItemAndTotal = []
+    let listOfItem = setItemList(bidderList)
+
+    for (let i = 0; i < listOfItem.length; i++) {
+        let listOfItemAmount = []
+        for (let j = 0; j < bidderList.length; j++) {
+            if (listOfItem[i] === bidderList[j].item) {
+                listOfItemAmount.push(bidderList[j].amount)
+            }
+        }
+        listOfItemAndTotal[i] = { [listOfItem[i]]: getArrayTotal(listOfItemAmount) }
+    }
+    return listOfItemAndTotal
+    
+}
+
+function getArrayTotal(array) {
+    return array.reduce((acc, element) => acc += element, 0)
+}
+// Filter bidder by its attributes
+function filterBid(bidderList, filterBy) {
+    return bidderList.map(element => element[filterBy])
+}
+
+// Group bids by item 
+function groupBids(bidderList, groupBy) {
+    let listOfItemAndGroupBy = []
+    let listOfItem = setItemList(bidderList)
+
+    for (let i = 0; i < listOfItem.length; i++) {
+        let listOfItemGroup = []
+        for (let j = 0; j < bidderList.length; j++) {
+            if(listOfItem[i] === bidderList[j].item) {
+                listOfItemGroup.push(bidderList[j][groupBy])
+            }
+
+        }
+        listOfItemAndGroupBy.push({[listOfItem[i]]: listOfItemGroup})
+    }
+    return listOfItemAndGroupBy
 }
 
 // Asynchronous call to fetch bidders 
 async function fetchBidders() {
     const response = await fetch("bidders.json")
-    const data = await response.text()
-    return JSON.parse(data)
+    const data = await response.json()
+    return data
 }
 
-const dataFetch = async () => {
+const loadData = async () => {
+    const temporaryList = []
     const data = await fetchBidders()
     data.bidder.forEach(element => {
-        bidderList.push(element)
+        temporaryList.push(element)
     })
+    return [...temporaryList]
 }
 
-console.log(bidderList);
 
+async function main() {
+    console.log(findHighestBid(await loadData()))
+    console.log(calculateTotalBids(await loadData()));
+    console.log(filterBid(await loadData(), "item"));
+    console.log(groupBids(await loadData(), "amount"));
+    
+}
+
+main()
